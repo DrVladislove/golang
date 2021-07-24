71d5dd04545eff64efe3d72203aacc49e1cb17ac
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2021-07-24 15:43:20 +0300

    rename variable

--- 08-numbers-and-strings/02-strings/exercises/08-right-trim-it/main.go
+++ 08-numbers-and-strings/02-strings/exercises/08-right-trim-it/main.go
@@ -9,6 +9,6 @@ import (
 func main() {
 	name := "inanç           "
 	name = strings.TrimRight(name, " ")
-	len := utf8.RuneCountInString(name)
+	length := utf8.RuneCountInString(name)
 	fmt.Println(len)
 }
